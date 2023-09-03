f781e4bdcfbc041b3ca646d33a910cb006cabef4
SatyaKarthik <satyakarthik2020@gmail.com>
2023-09-03 09:50:17 +0530

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+  const[content,setcontent]=useState([])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input placeholder='click from below' value={content}/><button> =</button><hr/>
+      <button onClick={() => setcontent((content)=>[...content,'+'])}>+</button>
+      <button>-</button>
+      <button onClick={() => setcontent([...content,'4'])}>4</button>
+    </>
   );
 }
-
-export default App;
